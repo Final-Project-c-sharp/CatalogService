17a6f5c157b07bf95bdc6f0f840fef435b40407d
Danya Boisan <boberiosif@gmail.com>
2020-12-17 16:12:15 +0200

    Source code
    code

---
+++ ProductCatalog/ProductCatalog/GroupsModels/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+namespace ProductCatalog.GroupsModels
+{
+    [DataContract]
+    class Manager
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string SurName { get; set; }
+        [DataMember]
+        public string PhoneNumber { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+        [DataMember]
+        public string Login { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+    }
+}
---
+++ ProductCatalog/ProductCatalog/UI_Menu/ClientMenu.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductCatalog.UI_Menu
+{
+    class ClientMenu
+    {
+    }
+}
